08cc5155057c36af521bb3fa16d5cb7ea186953b
Lucas-Dinelli <dinealbuquerque10@gmail.com>
2021-09-17 11:07:00 -0300

    Repository annotation

--- src/main/java/com/sigabem/frete/repository/FreteRepository.java
+++ src/main/java/com/sigabem/frete/repository/FreteRepository.java
@@ -1,9 +1,11 @@
 package com.sigabem.frete.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sigabem.frete.model.Frete;
 
+@Repository
 public interface FreteRepository extends JpaRepository<Frete, Long> {
 	
 }
